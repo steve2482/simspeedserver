9b6153b76af07c9c2bfeeff1d6df21732d9e1a75
steve2482 <stevecb12684@yahoo.com>
2017-05-22 15:20:06 -0400

    user registration completed

--- models/user.js
+++ models/user.js
@@ -1,13 +1,38 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-// user schema
-const userSchema = mongoose.Schema({
+// User Schema
+const UserSchema = mongoose.Schema({
   name: String,
+  email: String,
   userName: String,
   password: String,
-  favoritedChannels: Array
+  favoriteChannels: Array
 });
 
-const User = mongoose.model('User', userSchema);
+const User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports.createUser = function(newUser, callback) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+  let query = {userName: username};
+  User.findOne(query, callback);
+};
+
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+};
+
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    if (err) throw err;
+    callback(null, isMatch);
+  });
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fetch = require('isomorphic-fetch');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const expressValidator = require('express-validator');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -24,10 +25,122 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const {PORT, DATABASE_URL} = require('./config');
 const {Channel} = require('./models/channel');
+const User = require('./models/user');
 
 // ===========================================================================
 // PASSPORT SETUP=============================================================
 // ===========================================================================
+
+// Express Validator==========================================================
+// ===========================================================================
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+ 
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.post('/register', (req, res) => {
+  console.log(req.body);
+  let name = req.body.name;
+  let email = req.body.email;
+  let userName = req.body.userName;
+  let password = req.body.password;
+  let password2 = req.body.password2;
+
+  // Validation=======================================================
+  // =================================================================
+  req.checkBody('name', 'Name is Required').notEmpty();
+  req.checkBody('email', 'Email is Required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('userName', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+    res.status(400).json(errors);
+  } else {
+    let newUser = new User({
+      name: name,
+      email: email,
+      userName: userName,
+      password: password,
+      favoriteChannels: []
+    });
+    User.createUser(newUser, function(err, user) {
+      if (err) throw err;
+      req.login(user, function(err) {
+        if (err) {
+          throw err;
+        } else {
+          res.status(200).json('Registration Sucessful');
+        }
+      });
+    });
+  }
+});
+
+// Sign in strategy===================================================
+// ===================================================================
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.getUserByUsername(username, function(err, user) {
+      if (err) {
+        throw err;
+      }
+      if (!user) {
+        return done(null, false, {message: 'Unknown user'});
+      }
+      User.comparePassword(password, user.password, function(err, isMatch) {
+        if (err) {
+          throw err;
+        }
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Invalid password'});
+        }
+      });
+    });
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// User Login=========================================================
+// ===================================================================
+app.post('/login',
+  passport.authenticate('local', {successRedirect: '/', failureRedirect: '/users/login', failureFlash: true}),
+  function(req, res) {
+    let isLoggedIn = !!req.user;
+    res.redirect('/', {loggedIn: isLoggedIn});
+  });
+
+// User Logout
+app.get('/logout', function(req, res) {
+  req.logout();
+  req.flash('success_msg', 'You have logged out');
+  res.redirect('/users/login');
+});
 
 // ===========================================================================
 // APPLICATION API============================================================
